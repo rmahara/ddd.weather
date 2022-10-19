0f0a72306fb2ca0cb9ec016b3524576b317472f1
rmahara <rmahara@pacific-systems.co.jp>
2022-10-19 19:13:53 +0900

    #12_テストコードの書き方

---
+++ DDD/DDDTest.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using DDD.WinForm.Common;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var val = Class1.Add(1, 2);
+            Assert.AreEqual(3, val);
+        }
+    }
+}
