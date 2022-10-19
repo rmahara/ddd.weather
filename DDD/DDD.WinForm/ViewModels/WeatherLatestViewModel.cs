6de2926423f4a70f1efe989673d0c3c8a2a4de6f
rmahara <rmahara@pacific-systems.co.jp>
2022-10-19 19:37:26 +0900

    #17-2_最初のテスト

---
+++ DDD/DDD.WinForm/ViewModels/WeatherLatestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.WinForm.ViewModels
+{
+    public class WeatherLatestViewModel
+    {
+        public string AreaIdText { get; set; } = string.Empty;
+        public string DataDateText { get; set; } = string.Empty;
+        public string ConditionText { get; set; } = string.Empty;
+        public string TemperatureText { get; set; } = string.Empty;
+    }
+}
---
+++ DDD/DDDTest.Tests/WeatherLatestViewModelTest.cs
@@ -0,0 +1,20 @@
+﻿using DDD.WinForm.Common;
+using DDD.WinForm.ViewModels;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class WeatherLatestViewModelTest
+    {
+        [TestMethod]
+        public void シナリオ()
+        {
+            var viewModel = new WeatherLatestViewModel();
+
+            Assert.AreEqual("", viewModel.AreaIdText);
+            Assert.AreEqual("", viewModel.DataDateText);
+            Assert.AreEqual("", viewModel.ConditionText);
+            Assert.AreEqual("", viewModel.TemperatureText);
+        }
+    }
+}
