1f28567257fb42beb421f0012e5f78bd6dd7d1e6
rmahara <rmahara@pacific-systems.co.jp>
2022-10-24 19:28:34 +0900

    #34-2_Save画面の実装 ・WeatherSaveViewModelTestを追加 ・ViewModelを追加 ・画面起動時、DateTime.Nowの初期値のテスト方法に入る前

---
+++ DDD/DDD.WinForm/ViewModels/WeatherSaveViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DDD.WinForm.ViewModels
+{
+    public class WeatherSaveViewModel : ViewModelBase
+    {
+        public WeatherSaveViewModel()
+        {
+            DataDateValue = DateTime.Now;
+        }
+
+        public object SelectedAreaId { get; set; }
+        public DateTime DataDateValue { get; set; }
+        public object SelectedCondition { get; set; }
+        public string TemperatureTest { get; set; }
+    }
+}
---
+++ DDD/DDDTest.Tests/WeatherSaveViewModelTest.cs
@@ -0,0 +1,20 @@
+﻿using ChainingAssertion;
+using DDD.WinForm.ViewModels;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class WeatherSaveViewModelTest
+    {
+        [TestMethod]
+        public void 天気登録シナリオ() 
+        {
+            var viewModel = new WeatherSaveViewModel();
+            viewModel.SelectedAreaId.IsNull();
+            viewModel.DataDateValue.Is(Convert.ToDateTime("2018/1/1 12:34:56"));
+            viewModel.SelectedCondition.Is(1);
+            viewModel.TemperatureTest.Is("");
+
+        }
+    }
+}
